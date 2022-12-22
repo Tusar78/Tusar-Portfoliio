c1c18fae8bf9ea870880b77b43654bf0b56c2cd7
Tusar <tusarahmed141@gmail.com>
2022-12-22 16:18:20 +0600

    css markup with added meaningfull css classes

--- frontend-react/src/Components/Navbar/Navbar.jsx
+++ frontend-react/src/Components/Navbar/Navbar.jsx
@@ -4,10 +4,19 @@ import { images } from '../../Constants'
 
 const Navbar = () => {
   return (
-    <nav>
-      <div>
+    <nav className='app__navbar'>
+      <div className='app__navbar-logo'>
         <img src={ images.logo } alt="Website Logo" />
       </div>
+      <ul className='app__navbar-links'>
+        {
+          ['Home', 'About', 'Work', 'Skills', 'Contact'].map((item) => (
+            <li className='app__flex p-text' key={`link-${item}`}>
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          ))
+        }
+      </ul>
     </nav>
   )
 }
