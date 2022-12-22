b07fbbb5a0b017b9cb8b498b24f7e89d9044db0b
Tusar <tusarahmed141@gmail.com>
2022-12-22 13:15:44 +0600

    import neccessary component using traditional react way

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import About from './Container/About/About';
+import Footer from './Container/Footer/Footer';
+import Header from './Container/Header/Header';
+import Skills from './Container/Skills/Skills';
+import Testimonial from './Container/Testimonial/Testimonial';
+import Work from './Container/Work/Work';
 
 const App = () => {
   return (
-    <h1>Hello World</h1>
+    <div className='container'>
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
   );
 };
 
