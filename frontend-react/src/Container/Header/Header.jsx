9afe04ad3109ad7ba741e7bbbb82019943e8e580
Tusar <tusarahmed141@gmail.com>
2022-12-23 23:50:30 +0600

    I am going to be stay some time on header component

--- frontend-react/src/Container/Header/Header.jsx
+++ frontend-react/src/Container/Header/Header.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import './Header.scss'
+import React from "react";
+import { motion } from "framer-motion";
+import { images } from "../../Constants";
+import "./Header.scss";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+  return <div>Header</div>;
+};
 
-export default Header
+export default Header;
