96fc8f89e0653ce494483337a8e011c27d96dadd
Tusar <tusarahmed141@gmail.com>
2022-12-23 19:09:10 +0600

    Toggle markup done

--- frontend-react/src/Components/Navbar/Navbar.jsx
+++ frontend-react/src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,49 @@
-import React from 'react'
-import './Navbar.scss'
-import { images } from '../../Constants'
+import React, { useState } from "react";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
+import "./Navbar.scss";
+import { images } from "../../Constants";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(true);
   return (
-    <nav className='app__navbar'>
-      <div className='app__navbar-logo'>
-        <img src={ images.logo } alt="Website Logo" />
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.logo} alt="Website Logo" />
       </div>
-      <ul className='app__navbar-links'>
-        {
-          ['Home', 'About', 'Work', 'Skills', 'Contact'].map((item) => (
-            <li className='app__flex p-text' key={`link-${item}`}>
-              <div />
-              <a href={`#${item}`}>{item}</a>
-            </li>
-          ))
-        }
+      <ul className="app__navbar-links">
+        {["Home", "About", "Work", "Skills", "Contact"].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
       </ul>
-    </nav>
-  )
-}
+      <div className="app__navbar-menu">
+        <HiMenuAlt4
+          onClick={() => {
+            setToggle(true);
+          }}
+        />
 
-export default Navbar
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            {["Home", "About", "Work", "Skills", "Contact"].map((item) => (
+              <li key={item}>
+                <a href={`#${item}`} onClick={() => setToggle(false)}>
+                  {item}
+                </a>
+              </li>
+            ))}
+          </motion.div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
