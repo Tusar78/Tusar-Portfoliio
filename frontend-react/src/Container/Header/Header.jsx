0ef511e497757f22d6d52a490b8ab402364ca47d
Tusar <tusarahmed141@gmail.com>
2022-12-28 11:53:57 +0600

    Added some picture on motion divb

--- frontend-react/src/Container/Header/Header.jsx
+++ frontend-react/src/Container/Header/Header.jsx
@@ -3,6 +3,17 @@ import { motion } from "framer-motion";
 import { images } from "../../Constants";
 import "./Header.scss";
 
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut'
+    }
+  }
+}
+
 const Header = () => {
   return (
     <div className="app__header app__flex">
@@ -40,7 +51,17 @@ const Header = () => {
           alt="profile__cirlce"
         />
       </motion.div>
-      <motion.div></motion.div>
+      <motion.div
+        variants={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className="app__header-circles"
+      >
+        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+          <div className="circle-cmp app__flex" key={`circle-${index}`}>
+            <img src={circle} alt="circle" />
+          </div>
+        ))}
+      </motion.div>
     </div>
   );
 };
